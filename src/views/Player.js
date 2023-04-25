6e0144b2455d4e6d2d62b2bee43e0e9a70e06f5f
ioni5 <ioni5.goto@gmail.com>
2023-04-25 10:52:16 -0500

    add animations #5

--- src/views/Player.js
+++ src/views/Player.js
@@ -10,5 +10,42 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.setBounce(0.2)
     this.setCollideWorldBounds(true)
     this.body.setGravityY(300)
+    this.addAnimations()
+  }
+
+  addAnimations() {
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'player', frame: 0 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 4, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 9 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('dude', { start: 8, end: 9 }),
+        frameRate: 10,
+        repeat: -1
+    });
   }
 }
